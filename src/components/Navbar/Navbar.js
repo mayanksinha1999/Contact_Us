091bc2e1adbd526c02ce35dfa66f36b9ae392a0e
mayanksinha1999 <51639109+mayanksinha1999@users.noreply.github.com>
2023-06-24 17:36:28 +0530

    first commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styles from './Navbar.module.css';
+
+const Navbar = () => {
+  console.log(styles);
+  return (
+    <nav className={`${styles.navigation} container`}>
+        <div className="logo">
+            <img src="/images/logo.png" alt="do some coding logo" />
+        </div>
+
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
